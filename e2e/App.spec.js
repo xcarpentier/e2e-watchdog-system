7ebbf6c383196a1da4903365f60b9818b489e27d
Xavier Carpentier <xcapetir@gmail.com>
2018-11-28 18:51:08 +0100

    add video

--- e2e/App.spec.js
+++ e2e/App.spec.js
@@ -1,33 +1,21 @@
 const TIMEOUT = 120000
 
 describe('App', () => {
-  it('should display login screen', async () => {
+  it('should display login', async () => {
     await waitFor(element(by.label('Login screen')))
       .toBeVisible()
       .withTimeout(TIMEOUT)
-  })
 
-  it('should type email', async () => {
     const emailField = element(by.label('Fill your email')).atIndex(0)
     await expect(emailField).toBeVisible()
     await emailField.tap()
     await emailField.typeText('xcapetir@gmail.com')
-  })
 
-  it('should type password', async () => {
     const passwordField = element(by.label('Fill your password')).atIndex(0)
     await expect(passwordField).toBeVisible()
     await passwordField.tap()
     await passwordField.typeText('CwzLKz2EjM9u')
-  })
 
-  it('should sign in', async () => {
-    const signInButton = element(by.label('Submit to sign in'))
-    await expect(signInButton).toBeVisible()
-    await signInButton.tap()
-  })
-
-  it('should sign in', async () => {
     const signInButton = element(by.label('Submit to sign in'))
     await expect(signInButton).toBeVisible()
     await signInButton.tap()
