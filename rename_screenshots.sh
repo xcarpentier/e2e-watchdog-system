3039ff67b68f882f01b18c484727496745bdebc6
Xavier Carpentier <xcapetir@gmail.com>
2018-12-04 13:26:58 +0100

    chore show only after screenshot

--- rename_screenshots.sh
+++ rename_screenshots.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 timestamp=`date "+%Y-%m-%d-%H-%M"`
-find ./artifacts/ -type d -not -empty -exec mv \{\}/beforeEach.png \{\}_1_$timestamp.png \;
+# find ./artifacts/ -type d -not -empty -exec mv \{\}/beforeEach.png \{\}_1_$timestamp.png \;
 find ./artifacts/ -type d -not -empty -exec mv \{\}/afterEach.png \{\}_2_$timestamp.png \;
