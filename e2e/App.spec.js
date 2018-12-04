51e328ec114a766d4d571c539de9b708b521cfc7
Xavier Carpentier <xcapetir@gmail.com>
2018-12-04 11:09:18 +0100

    add new e2e tests

--- e2e/App.spec.js
+++ e2e/App.spec.js
@@ -16,25 +16,54 @@ describe('App', () => {
   it('1 should fill email', async () => {
     const emailField = element(by.label('Fill your email')).atIndex(0)
     await expect(emailField).toBeVisible()
-    await emailField.tap()
-    await emailField.typeText('xcapetir@gmail.com')
+    await emailField.replaceText('xcapetir@gmail.com')
   })
 
   it('2 should fill password', async () => {
     const passwordField = element(by.label('Fill your password')).atIndex(0)
-    await expect(passwordField).toBeVisible()
-    await passwordField.tap()
-    await passwordField.typeText('CwzLKz2EjM9u')
+    await passwordField.replaceText('CwzLKz2EjM9u')
   })
 
   it('3 should submit sign in form', async () => {
     const signInButton = element(by.label('Submit to sign in'))
-    await expect(signInButton).toBeVisible()
+    await expect(signInButton).toExist()
     await signInButton.tap()
   })
 
   it('4 should display map', async () => {
     await waitFor(element(by.label('This is the WATCHDOG map')))
+      .toBeVisible()
+      .withTimeout(TIMEOUT)
+  })
+
+  it('5 should tap to open menu', async () => {
+    const drawerIcon = element(by.label('Tap to open the menu'))
+    await expect(drawerIcon).toBeVisible()
+    await drawerIcon.tap()
+  })
+
+  it('6 should show menu', async () => {
+    await waitFor(element(by.label('Menu opened')))
+      .toBeVisible()
+      .withTimeout(TIMEOUT)
+  })
+
+  it('7 should show go to my account', async () => {
+    const goToMyAccount = element(by.label('Go to my account'))
+    await expect(goToMyAccount).toBeVisible()
+    await goToMyAccount.tap()
+  })
+
+  it('8 should log out', async () => {
+    const logMeOut = element(by.label('Log me out'))
+    await waitFor(logMeOut)
+      .toBeVisible()
+      .withTimeout(TIMEOUT)
+    await logMeOut.tap()
+  })
+
+  it('9 should show login screen after logout', async () => {
+    await waitFor(element(by.label('Login screen')))
       .toBeVisible()
       .withTimeout(TIMEOUT)
   })
--- rename_screenshots.sh
+++ rename_screenshots.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-find ./artifacts/ -type d -not -empty -exec mv \{\}/beforeEach.png \{\}_1.png \;
-find ./artifacts/ -type d -not -empty -exec mv \{\}/afterEach.png \{\}_2.png \;
+timestamp=`date "+%Y-%m-%d-%H-%M"`
+find ./artifacts/ -type d -not -empty -exec mv \{\}/beforeEach.png \{\}_1_$timestamp.png \;
+find ./artifacts/ -type d -not -empty -exec mv \{\}/afterEach.png \{\}_2_$timestamp.png \;
