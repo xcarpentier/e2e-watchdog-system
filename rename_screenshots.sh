3eb3cb759811b5391c2d70e93a9b28acbef77092
Xavier Carpentier <xcapetir@gmail.com>
2018-12-07 11:03:31 +0100

    fix add order in artifact

--- rename_screenshots.sh
+++ rename_screenshots.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find ./artifacts/ -type d -not -empty -exec mv \{\}/beforeEach.png \{\}_before_$TRAVIS_BUILD_NUMBER.png \;
-find ./artifacts/ -type d -not -empty -exec mv \{\}/afterEach.png \{\}_after_$TRAVIS_BUILD_NUMBER.png \;
+find ./artifacts/ -type d -not -empty -exec mv \{\}/beforeEach.png \{\}_0_before_$TRAVIS_BUILD_NUMBER.png \;
+find ./artifacts/ -type d -not -empty -exec mv \{\}/afterEach.png \{\}_1_after_$TRAVIS_BUILD_NUMBER.png \;
