470c097d820f60a7d21dcd5213b2703ba267d5f7
Xavier Carpentier <xcapetir@gmail.com>
2018-12-07 10:31:03 +0100

    fix add more images

--- rename_screenshots.sh
+++ rename_screenshots.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-find ./artifacts/ -type d -not -empty -exec mv \{\}/afterEach.png \{\}_$TRAVIS_BUILD_NUMBER.png \;
+find ./artifacts/ -type d -not -empty -exec mv \{\}/beforeEach.png \{\}_before_$TRAVIS_BUILD_NUMBER.png \;
+find ./artifacts/ -type d -not -empty -exec mv \{\}/afterEach.png \{\}_after_$TRAVIS_BUILD_NUMBER.png \;
